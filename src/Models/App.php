9a77937b3fd6dcbcd2539e0ab0249e8186c7698f
JoseChan <524233828@qq.com>
2019-12-16 19:29:47 +0800

    增加获取当前token方法

--- src/Models/App.php
+++ src/Models/App.php
@@ -10,6 +10,7 @@ namespace JoseChan\App\DataSet\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Redis;
 use JoseChan\App\DataSet\Collection\AppCollection;
 
 /**
@@ -21,10 +22,18 @@ use JoseChan\App\DataSet\Collection\AppCollection;
  * @property string $app_secret
  * @property string $created_at
  * @property string $updated_at
+ * @property string $private_key
  * @property int $status
  */
 class App extends Model
 {
+
+    //redis key
+    const TOKEN_KEY = "app_token:";
+
+    //token过期时间
+    const EXPIRE_TIME = 7200;
+
     /**
      * 设置采用的集合类
      * @param array $models
@@ -43,9 +52,36 @@ class App extends Model
     {
         if($this->exists)
         {
-            return md5($this->id.$this->app_secret.time());
+            $key = $this->getTokenKey();
+            $token = md5($this->id.$this->app_secret.time());
+            //设置token
+            Redis::set($key, $token);
+            Redis::expire($key, self::EXPIRE_TIME);
+            return $token;
         }else{
             return false;
+        }
+    }
+
+    /**
+     * 获取应用当前的token
+     * @return mixed
+     */
+    public function getToken()
+    {
+        if($this->exists){
+            return Redis::get($this->getTokenKey());
+        }
+    }
+
+    /**
+     * 获取redis key
+     * @return string
+     */
+    public function getTokenKey()
+    {
+        if($this->exists){
+            return self::TOKEN_KEY. $this->id;
         }
     }
 }
